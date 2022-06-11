d3922f62a6e9f1cf7fc84ec4ed9bfed2d5e351e8
vasi1e <vaskosusanin@gmail.com>
2022-06-11 16:56:52 +0200

    Add script for creating database tables

---
+++ db/createTable.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE `table_anotator`;
+USE `table_anotator`;
+
+CREATE TABLE `users` (
+`id` INT NOT NULL AUTO_INCREMENT,
+`username` VARCHAR(50) NOT NULL UNIQUE,
+`password` VARCHAR(255) NOT NULL,
+PRIMARY KEY (`id`));
+
+CREATE TABLE `tables` (
+`id` INT NOT NULL AUTO_INCREMENT,
+`creatorID` INT NOT NULL,
+`rows` INT NOT NULL,
+`columns`INT NOT NULL, 
+`table` TEXT NOT NULL,
+PRIMARY KEY (`id`),
+FOREIGN KEY (`creatorID`) REFERENCES users(`id`));
