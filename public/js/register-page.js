81204ad49313ce07288ced2a66bbff19810f0c44
Hristo <HDoychev@technologica.com>
2022-06-12 15:54:54 +0300

    Промяна на register-page.html/js

--- public/js/register-page.js
+++ public/js/register-page.js
@@ -1,12 +1,4 @@
 'use strict';
-const NAME_ID = "name";
-const NAME_SECTION_ID = "name-section";
-const NAME_ERROR_LABEL = "labelForFirstName";
-const NAME_ERROR_MESSAGE = "First name is invalid!"
-const LAST_NAME_ID = "last-name";
-const LAST_NAME_SECTION_ID = "last-name-section";
-const LAST_NAME_ERROR_LABEL = "labelForLastName";
-const LAST_NAME_ERROR_MESSAGE = "Last name is invalid!";
 const EMAIL_ID = "email";
 const EMAIL_SECTION_ID = "email-section";
 const EMAIL_ERROR_LABEL = "labelForEmail";
@@ -21,12 +13,10 @@ const CONFIRM_PASSWORD_ERROR_LABEL = "labelForConfirmPassword";
 const CONFIRM_PASSWORD_ERROR_MESSAGE = "Passwords must match!";
 
 function validateAndShowErrorMessageIfNeeded() {
-    const isFirstNameValid = validateFirstName();
-    const isLastNameValid = validateLastName();
     const isEmailValid = validateEmail();
     const isPasswordValid = validatePassword();
     const isPasswordConfirmed = validateConfirmPassword();
-    if (isFirstNameValid && isLastNameValid && isEmailValid && isPasswordValid && isPasswordConfirmed) {
+    if (isEmailValid && isPasswordValid && isPasswordConfirmed) {
         const data = {
             'email': document.getElementById("email").value,
             'password': document.getElementById("password").value,
@@ -48,29 +38,6 @@ function validateAndShowErrorMessageIfNeeded() {
 
 }
 
-function validateFirstName() {
-    const firstNameRegEx = /^[a-zA-Z'\-]{2,30}$/;
-    const firstName = document.getElementById(NAME_ID).value;
-    if (!firstNameRegEx.test(firstName)) {
-        addLabelForWrongInput(NAME_ERROR_LABEL, NAME_ERROR_MESSAGE, NAME_ID, NAME_SECTION_ID);
-        return false;
-    } else {
-        removeLabelForWrongInput(NAME_ERROR_LABEL, NAME_ID);
-        return true;
-    }
-}
-
-function validateLastName() {
-    const lastNameRegEx = /^[a-zA-Z'\-]{2,30}$/;
-    const lastName = document.getElementById(LAST_NAME_ID).value;
-    if (!lastNameRegEx.test(lastName)) {
-        addLabelForWrongInput(LAST_NAME_ERROR_LABEL, LAST_NAME_ERROR_MESSAGE, LAST_NAME_ID, LAST_NAME_SECTION_ID);
-        return false;
-    } else {
-        removeLabelForWrongInput(LAST_NAME_ERROR_LABEL, LAST_NAME_ID);
-        return true;
-    }
-}
 
 function validateEmail() {
     const emailAddressPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
